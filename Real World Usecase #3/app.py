bb2dbf03c365a80d0f279f1ae5537321d7ab8f86
Pradnya Deshpande <pradnyadesh10@gmail.com>
2025-11-03 20:18:07 +0530

    Add files via upload

--- Real World Usecase #3/app.py
+++ Real World Usecase #3/app.py
@@ -1,1 +1,20 @@
+from flask import Flask 
+import redis 
 
+app = Flask(__name__)
+
+# Connect to Redis (container name from docker-compose)
+client = redis.Redis(host='redis-server', port=6379)
+
+# Initialize visits
+client.set('visits', 0)
+
+@app.route('/')
+def index():
+    visits = client.get('visits')
+    visits = int(visits) if visits else 0
+    client.set('visits', visits + 1)
+    return f"Members Visited : {visits}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8081)
