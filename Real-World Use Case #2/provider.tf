78c9f458c51c35ecb02ace37f5ce17dab703e8aa
Pradnya Deshpande <pradnyadesh10@gmail.com>
2025-11-19 19:44:21 +0530

    Update provider.tf with azurerm credentials placeholders

--- Real-World Use Case #2/provider.tf
+++ Real-World Use Case #2/provider.tf
@@ -17,8 +17,11 @@ provider "docker" {
 
 provider "azurerm" {
   features {  }
-  subscription_id = "a96db7f9-f077-48d6-9d2e-68075185bc77"
-  client_id = "65d199e5-f822-4b65-97a3-9c1b83894ed4"
-  client_secret = "sIv8Q~Ajj4V4JVRMebGHfxVs-6WaJtcwElJFhdB9"
-  tenant_id = "b3e33edc-ced7-433a-9f35-93df9317d7a4"
+
+#Add subscription ID,client ID, secret and tenant ID
+  subscription_id = ""
+  client_id = ""
+  client_secret = ""
+  tenant_id = ""
+
 }
